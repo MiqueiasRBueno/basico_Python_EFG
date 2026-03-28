1107830cc2c158bcfe183e0a6c77d87e7b4d37e6
Miquéias <160036158+MiqueiasRBueno@users.noreply.github.com>
2026-03-28 01:17:18 +0000

    EX01 Analisador de números positivo!

---
+++ analis_numero.py
@@ -0,0 +1,18 @@
+# 1 - Construa um programa que seja capaz de pedir um número ao usuário, repetindo a pergunta caso o número
+#  seja negativo e mostrando uma mensagem de sucesso caso seja positivo;
+
+num_pos_usu = int(input("Digite um número inteiro positivo: "))
+while True:
+    if num_pos_usu < 0:
+        print(f"""Você digitou o número {num_pos_usu}.
+O número digitado é um número negativo!""")
+        num_pos_usu = int(input("Digite novamente: "))
+    elif num_pos_usu == 0:
+        print("Você digitou o número 0!")
+        num_pos_usu = int(input("Digite novamente: "))
+    else:
+        if num_pos_usu > 0:
+            print(f"""Você digitou o número {num_pos_usu}
+Este é um número positivo, parabéns!""")
+            break
+            
